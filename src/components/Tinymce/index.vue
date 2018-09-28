bfe2b43b68ef575482c4d80def9f1e9ef89ed463
xiaoxu.nie <xiaoxu.nie@zhangmen.com>
2018-09-28 16:02:11 +0800

    修改tinymce全屏bug

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -14,11 +14,11 @@
 
   export default {
     name: 'Tinymce',
-    components: {editorImage},
+    components: { editorImage },
     props: {
       id: {
         type: String,
-        default: function () {
+        default: function() {
           return 'vue-tinymce-' + +new Date() + ((Math.random() * 1000).toFixed(0) + '')
         }
       },
@@ -106,7 +106,6 @@
           default_link_target: '_blank',
           link_title: false,
           nonbreaking_force_tab: true, // inserting nonbreaking space &nbsp; need Nonbreaking Space Plugin
-          //images_upload_url: '/oss/file/uploadRichImg?dir=image',
           init_instance_callback: editor => {
             if (_this.value) {
               editor.setContent(_this.value)
@@ -121,40 +120,40 @@
             editor.on('FullscreenStateChanged', (e) => {
               _this.fullscreen = e.state
             })
-          },
-          //整合七牛上传
-          /*images_dataimg_filter(img) {
-            setTimeout(() => {
-              const $image = $(img);
-              $image.removeAttr('width');
-              $image.removeAttr('height');
-              if ($image[0].height && $image[0].width) {
-                $image.attr('data-wscntype', 'image');
-                $image.attr('data-wscnh', $image[0].height);
-                $image.attr('data-wscnw', $image[0].width);
-                $image.addClass('wscnph');
-              }
-            }, 0);
-            return img
-          },
-          images_upload_handler(blobInfo, success, failure, progress) {
-            progress(0);
-            const token = _this.$store.getters.token;
-            getToken(token).then(response => {
-              const url = response.data.qiniu_url;
-              const formData = new FormData();
-              formData.append('token', response.data.qiniu_token);
-              formData.append('key', response.data.qiniu_key);
-              formData.append('file', blobInfo.blob(), url);
-              upload(formData).then(() => {
-                success(url);
-                progress(100);
-              })
-            }).catch(err => {
-              failure('出现未知问题，刷新页面，或者联系程序员')
-              console.log(err);
-            });
-          }*/
+          }
+          // 整合七牛上传
+          // images_dataimg_filter(img) {
+          //   setTimeout(() => {
+          //     const $image = $(img);
+          //     $image.removeAttr('width');
+          //     $image.removeAttr('height');
+          //     if ($image[0].height && $image[0].width) {
+          //       $image.attr('data-wscntype', 'image');
+          //       $image.attr('data-wscnh', $image[0].height);
+          //       $image.attr('data-wscnw', $image[0].width);
+          //       $image.addClass('wscnph');
+          //     }
+          //   }, 0);
+          //   return img
+          // },
+          // images_upload_handler(blobInfo, success, failure, progress) {
+          //   progress(0);
+          //   const token = _this.$store.getters.token;
+          //   getToken(token).then(response => {
+          //     const url = response.data.qiniu_url;
+          //     const formData = new FormData();
+          //     formData.append('token', response.data.qiniu_token);
+          //     formData.append('key', response.data.qiniu_key);
+          //     formData.append('file', blobInfo.blob(), url);
+          //     upload(formData).then(() => {
+          //       success(url);
+          //       progress(100);
+          //     })
+          //   }).catch(err => {
+          //     failure('出现未知问题，刷新页面，或者联系程序员')
+          //     console.log(err);
+          //   });
+          // },
         })
       },
       destroyTinymce() {
@@ -182,28 +181,23 @@
   .tinymce-container {
     position: relative;
   }
-
-  .tinymce-container > > > .mce-fullscreen {
+  .tinymce-container>>>.mce-fullscreen {
     z-index: 10000;
   }
-
   .tinymce-textarea {
     visibility: hidden;
     z-index: -1;
   }
-
   .editor-custom-btn-container {
     position: absolute;
     right: 4px;
     top: 4px;
     /*z-index: 2005;*/
   }
-
   .fullscreen .editor-custom-btn-container {
     z-index: 10000;
     position: fixed;
   }
-
   .editor-upload-btn {
     display: inline-block;
   }
